adb7fac007fbb7302e04fe611bf893156f2f7aa1
haykam821 <24855774+haykam821@users.noreply.github.com>
2020-05-10 12:46:43 -0400

    Update mod dependencies

--- src/main/java/io/github/haykam821/surrogates/Main.java
+++ src/main/java/io/github/haykam821/surrogates/Main.java
@@ -1,7 +1,7 @@
 package io.github.haykam821.surrogates;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.block.FabricBlockSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.BlockItem;
@@ -13,7 +13,7 @@ import net.minecraft.util.registry.Registry;
 public class Main implements ModInitializer {
 	private static final Identifier ENDER_SURROGATE_ID = new Identifier("surrogates", "ender_surrogate");
 
-	public static final Block ENDER_SURROGATE = new SurrogateBlock(FabricBlockSettings.copy(Blocks.END_STONE).build());
+	public static final Block ENDER_SURROGATE = new SurrogateBlock(FabricBlockSettings.copy(Blocks.END_STONE));
 	public static final Item ENDER_SURROGATE_ITEM = new BlockItem(ENDER_SURROGATE, new Item.Settings().group(ItemGroup.REDSTONE));
 
 	@Override
